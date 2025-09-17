bc6076a27c9b04a54982439fcc4aef4e9539807d
Saimandsroy <kumarsaimand@gmail.com>
2025-09-17 06:48:08 +0530

    Initial commit

---
+++ src/main/java/com/kuberfashion/backend/KuberFashionApplication.java
@@ -0,0 +1,13 @@
+package com.kuberfashion.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberFashionApplication.class, args);
+	}
+
+}
